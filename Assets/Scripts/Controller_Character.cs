e34ae8789788c2d64308ae8f214f93bb5f49c3c6
Martin Talha <mart97@live.no>
2022-10-11 17:49:37 +0200

    Part 1/2 - New proper collisions.

--- Assets/Scripts/Controller_Character.cs
+++ Assets/Scripts/Controller_Character.cs
@@ -67,7 +67,8 @@ public class Controller_Character : MonoBehaviour
             ControllMovement(timeStep);
             ControllCamera(timeStep);
 
-            transform.position += velocity * timeStep;
+            manuallyCollision(timeStep, velocity);
+            //transform.position += velocity * timeStep;
 
 
 
@@ -186,5 +187,78 @@ public class Controller_Character : MonoBehaviour
                 fallingInformatiom = "Fallspeed: " + currentFallspeed + " m/s.";
         }
 
+    }
+
+    void manuallyCollision(float timeStep, Vector3 currentVelocity)
+    {
+        CapsuleCollider capsule = GetComponent<CapsuleCollider>();
+
+        bool isGrounded = true;
+
+        float DistanceTimesSize = (currentVelocity.magnitude * timeStep) / (capsule.radius / 2); // The radius is divided by two to make it more accurate
+
+        //		if(DistanceTimesSize > 1)
+        //			Debug.LogWarning ("A player is moving at a speed of " +  Tools.RoundWithinReason(DistanceTimesSize, 2) * 50 + "% of their hitbox radius. (" + currentVelocity.magnitude * TimeStep + " u/tick)");
+
+
+        int totalMoveCount = (int)Mathf.Ceil(DistanceTimesSize);
+        if (totalMoveCount == 0)
+            totalMoveCount = 1;
+        int remainingMoveCount = totalMoveCount;
+
+        while (remainingMoveCount > 0) // As long as it's not moving more than the radius of the collider.
+        {
+            if (remainingMoveCount > 10)
+            {
+                Debug.LogWarning("WARNING: The Player is moving at an extreme speed. Skipping " + remainingMoveCount + " physics frames for object.");
+
+                remainingMoveCount = 0;
+                totalMoveCount = 1;
+            }
+
+            // Movement
+            remainingMoveCount -= 1;
+            transform.position += (currentVelocity * timeStep) / totalMoveCount;
+
+            // // Vertical location
+            //RaycastHit EndHit;
+            //Physics.Raycast(transform.position, -transform.up, out EndHit, (capsule.height / 2) + 1);
+
+            //bool hasContact = EndHit.transform != null;
+            //bool isBendingKnee = hasContact && EndHit.distance < capsule.height / 2;
+            //float groundDistance = EndHit.distance;
+            //bool isJustAbove = hasContact && isGrounded && groundDistance < capsule.height / 2 + CharacterSettings.GroundSnap;
+            //bool isAscending = Vector3.Dot(Vector3.Project(currentVelocity, CharacterSettings.Upwards), CharacterSettings.Upwards) > 0;
+            //
+            //if (isBendingKnee && timeStep != 0)
+            //    transform.position += transform.up * (CharacterSettings.PlayerHeight / 2 - EndHit.distance) * timeStep * stairSpeed;
+
+            // Collisions
+            Collider[] overlaps = new Collider[4];
+            LayerMask IgnoreLayer = ~new LayerMask();
+            Vector3 End = transform.position + transform.up * (capsule.height / 2 - capsule.radius);
+
+            int lenght = Physics.OverlapSphereNonAlloc(End, capsule.radius * 1.5f, overlaps, IgnoreLayer, QueryTriggerInteraction.Ignore);
+
+            for (int i = 0; i < lenght; i++)
+            {
+                Transform t = overlaps[i].transform;
+
+                if (t == transform)
+                    continue;
+
+                Vector3 dir;
+                float distance;
+
+                if (Physics.ComputePenetration(capsule, transform.position + transform.up * capsule.radius, transform.rotation, overlaps[i], t.position, t.rotation, out dir, out distance))
+                {
+                    dir = dir - Vector3.Project(dir, transform.up); // This is relative horizontal, not relative to gravity.
+
+                    transform.position = transform.position + dir * distance;
+
+                    velocity -= Vector3.Project(currentVelocity, dir); // Removes the velocity once impacting with a wall.
+                }
+            }
+        }
     }
 }
